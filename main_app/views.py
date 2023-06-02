c28bba0ea24a38588939bb07ea482bd451eb91a2
SaidNivek <kevin.vi.dias@gmail.com>
2023-06-02 17:33:40 -0400

    Import Cat model and make some changes ot the model

--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,16 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+# Add new view
+def cats_index(request):
+  # We pass data to a template very much like we did in Express!
+  return render(request, 'cats/index.html', {
+    'cats': cats
+  })
+
+# Add this cats list below the imports
+cats = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
