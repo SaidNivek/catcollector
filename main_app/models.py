46eddc844baf2f7f4d06e630ed880deebdaffd9d
SaidNivek <kevin.vi.dias@gmail.com>
2023-06-05 23:35:04 -0400

    Add Feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 # Import the reverse function
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 # Create your models here.
 class Cat(models.Model):
     name = models.CharField(max_length=100)
@@ -16,3 +22,21 @@ class Cat(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'cat_id': self.id})
+    
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(
+    max_length=1,
+    # add the 'choices' field option
+    choices=MEALS,
+    # set the default value for meal to be 'B'
+    default=MEALS[0][0]
+  )
+
+  # Create a cat_id FK
+  cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
+
